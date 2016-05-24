f6fe8f7465b00a50877a8b45d9a97b800407dca1
michelleRitzema <michelleritzema@hotmail.com>
2016-05-24 13:57:16 +0100

    test modules + room selector

---
+++ Assets/Modules/ModuleMain/Scripts/RoomSelector/RoomButtons.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Created by Michelle Ritzema.
+ * 
+ * Class that handles the button actions of the room selector panel.
+ */
+
+public class RoomButtons : MonoBehaviour
+{
+
+    public GameObject roomSelector, button;
+    public Material active, inactive, clicked;
+
+    /*
+     * Changes the button's texture when the mouse hovers over the button.
+     */
+    public void OnMouseEnter()
+    {
+        button.GetComponent<Renderer>().material = active;
+    }
+
+    /*
+     * Changes the button's texture when the mouse presses the button.
+     */
+    public void OnMouseDown()
+    {
+        button.GetComponent<Renderer>().material = clicked;
+        //passwordPanel.GetComponent<PasswordTerminal>().CheckPassword();
+    }
+
+    /*
+     * Changes the button's texture when the mouse is done pressing the button.
+     */
+    public void OnMouseUp()
+    {
+        button.GetComponent<Renderer>().material = active;
+    }
+
+    /*
+     * Changes the button's texture when the mouse no longer hovers over the button.
+     */
+    public void OnMouseExit()
+    {
+        button.GetComponent<Renderer>().material = inactive;
+    }
+
+}
