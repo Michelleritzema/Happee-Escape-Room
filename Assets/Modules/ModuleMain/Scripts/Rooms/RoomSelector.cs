bcea1c81242d3ee2e51a4a0d581f141694fee22b
michelleRitzema <michelleritzema@hotmail.com>
2016-06-02 22:52:44 +0100

    fixed unity editor error

--- Assets/Modules/ModuleMain/Scripts/Rooms/RoomSelector.cs
+++ Assets/Modules/ModuleMain/Scripts/Rooms/RoomSelector.cs
@@ -28,10 +28,10 @@ public class RoomSelector : MonoBehaviour {
         string room2 = game.GetComponent<Settings>().GetModule2();
         string room3 = game.GetComponent<Settings>().GetModule3();
         string room4 = game.GetComponent<Settings>().GetModule4();
-        string logo1Texture = "Assets/Modules/" + room1 + "/Logo/" + room1 + ".png";
-        string logo2Texture = "Assets/Modules/" + room2 + "/Logo/" + room2 + ".png";
-        string logo3Texture = "Assets/Modules/" + room3 + "/Logo/" + room3 + ".png";
-        string logo4Texture = "Assets/Modules/" + room4 + "/Logo/" + room4 + ".png";
+        string logo1Texture = "Logos/" + room1;
+        string logo2Texture = "Logos/" + room2;
+        string logo3Texture = "Logos/" + room3;
+        string logo4Texture = "Logos/" + room4;
         rooms[0] = GameObject.Find(room1);
         rooms[1] = GameObject.Find(room2);
         rooms[2] = GameObject.Find(room3);
@@ -40,10 +40,10 @@ public class RoomSelector : MonoBehaviour {
         rooms[1].transform.Find("DoneButton").transform.Find("Button").GetComponent<CompletionButtonActions>().SetRoom(1);
         rooms[2].transform.Find("DoneButton").transform.Find("Button").GetComponent<CompletionButtonActions>().SetRoom(2);
         rooms[3].transform.Find("DoneButton").transform.Find("Button").GetComponent<CompletionButtonActions>().SetRoom(3);
-        logo1 = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath(logo1Texture, typeof(Texture));
-        logo2 = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath(logo2Texture, typeof(Texture));
-        logo3 = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath(logo3Texture, typeof(Texture));
-        logo4 = (Texture)UnityEditor.AssetDatabase.LoadAssetAtPath(logo4Texture, typeof(Texture));
+        logo1 = Resources.Load<Texture>(logo1Texture);
+        logo2 = Resources.Load<Texture>(logo2Texture);
+        logo3 = Resources.Load<Texture>(logo3Texture);
+        logo4 = Resources.Load<Texture>(logo4Texture);
         MakeRoomActive(roomIndicator);
     }
 
