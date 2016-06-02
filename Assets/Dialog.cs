f9075e2e937f0f8b3c6f330e63f61f262c07dff1
Daslicey <sanderdeleng@outlook.com>
2016-06-02 14:22:23 +0200

    medicall stuff

---
+++ Assets/Dialog.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+    public string[] PatientText;
+    public string[] Questions;
+    bool DisplayText = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onGUI()
+    {
+        GUILayout.BeginArea(new Rect(700, 600, 400, 400));
+        if (DisplayText)
+        {
+            GUILayout.Label(PatientText[0]);
+            GUILayout.Button(Questions[0]);
+            GUILayout.Label(PatientText[1]);
+
+        }
+        GUILayout.EndArea();
+    }
+
+    void onTriggerEnter()
+    {
+        DisplayText = true;
+    }
+
+    void OnTriggerQuit()
+    {
+        DisplayText = false;
+    }
+}
